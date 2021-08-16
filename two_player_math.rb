50edf9db647234215b9cb87b028c0e89f6b90ceb
elizawtw <twew02@gmail.com>
2021-08-16 17:06:06 +0000

    planning

---
+++ two_player_math.rb
@@ -0,0 +1,25 @@
+=begin
+task 1: extract nouns for classes
+  class Game
+     
+  end
+  
+  class Players
+  end
+  
+  class Questions
+  end  
+
+task 2: role for each class
+Game:
+initialize with players
+track lives of players 
+announce winner
+
+Players:
+initialize with name
+start life = 3
+
+Questions:
+initialize with answer
+=end
